57261bb1ff15b553da229a7ec603eb9d69c0e4bd
Grosso05 <128441128+Grosso05@users.noreply.github.com>
2023-06-26 23:37:08 -0500

    Update

---
+++ Talleres4y5-main/myapp/src/main/java/co/edu/sena/myapp/model/BasicConnectionSingleton.java
@@ -0,0 +1,19 @@
+package co.edu.sena.myapp.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BasicConnectionSingleton {
+    private static String url = "jdbc:mysql://localhost:3306/myapp?serverTimezone=America/Bogota";
+    private static String user = "brayan";
+    private static String pass = "12345BrayAn";
+    private static Connection conn;
+    public static Connection getInstance()
+            throws SQLException {
+        if (conn == null) {
+            conn = DriverManager.getConnection(url, user, pass);
+        }
+        return conn;
+    }
+} // BasicConnectionSingleton
